4fe7b11bed2ad8017024800909a18cab83082a1a
keneanung <keneanung@googlemail.com>
2022-05-21 22:03:57 +0200

    Preliminar website

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Table from "react-bootstrap/Table";
+import Spinner from "react-bootstrap/Spinner";
 
-function App() {
+type RepositoryData = PackageData[];
+
+type PackageData = {
+  name: string;
+  packageName: string;
+  version: string;
+  description: string;
+  url: string;
+  dependencies: string[];
+};
+
+function RepositoryTable(props: { data: RepositoryData }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Package Name</th>
+          <th>Version</th>
+          <th>Description</th>
+          <th>Manual Download</th>
+          <th>Dependencies</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((packageData) => (
+          <tr key={packageData.name}>
+            <td>{packageData.name}</td>
+            <td>{packageData.packageName}</td>
+            <td>{packageData.version}</td>
+            <td>{packageData.description}</td>
+            <td>
+              <a style={{color: 'lightblue'}} href={packageData.url}>link</a>
+            </td>
+            <td>{packageData.dependencies.join(", ")}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
 
-export default App;
+export function App() {
+  const [loading, setLoading] = React.useState(true);
+  const [errorState, setErrorState] = React.useState(false);
+  const [repositories, setRepositories] = React.useState<RepositoryData>([]);
+
+  React.useEffect(() => {
+    fetch("repository.json")
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        setErrorState(true);
+      })
+      .then((data) => {
+        setRepositories(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorState(true);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header style={{color: 'white', textAlign: "center", fontSize: '22pt'}}>List of Nexus Packages available through this repository</header>
+      {loading ? (
+        <Spinner animation="border" role="status" variant="light">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : errorState ? (
+        <div>Error occured.</div>
+      ) : (
+        <RepositoryTable data={repositories} />
+      )}
+    </div>
+  );
+}
